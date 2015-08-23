8e6da395782bb11c80a6ce54b6c5701151cc6c1b
lordjancso <lordjancso@gmail.com>
2015-08-23 11:53:58 +0200

    Pagination added and sitebuild improved

--- src/AppBundle/Controller/StudentController.php
+++ src/AppBundle/Controller/StudentController.php
@@ -3,15 +3,23 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class StudentController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager('default');
 
         $study_groups = $em->getRepository('AppBundle:StudyGroup')->findAll();
         $students = $em->getRepository('AppBundle:Student')->findAll();
+
+        $paginator = $this->get('knp_paginator');
+        $students = $paginator->paginate(
+            $students,
+            $request->query->getInt('page', 1),
+            25
+        );
 
         return $this->render('student/index.html.twig', array(
             'study_groups' => $study_groups,
