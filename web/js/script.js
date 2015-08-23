f33defce263af023ec54a7e294a859de34471ff2
lordjancso <lordjancso@gmail.com>
2015-08-23 15:47:51 +0200

    Bulk delete functionality added

--- web/js/script.js
+++ web/js/script.js
@@ -1,21 +1,60 @@
 $(function () {
     $('input[name="students[]"]').on('change', function () {
-        var ids_input = $('#delete_form input');
-        var ids = ids_input.val().split(',');
         var id = $(this).val();
 
         if ($(this).is(':checked')) {
-            ids.push(id);
+            addId(id);
         } else {
-            ids = $.grep(ids, function (value) {
-                return value != id;
-            });
-        }
+            if ($('#all_students').is(':checked')) {
+                $('#all_students').prop('checked', false);
+            }
 
-        ids_input.val(ids.join(','));
+            removeId(id);
+        }
     });
 
     $('#filter input').on('change', function () {
         $(this).closest('form').submit();
     });
+
+    $('#all_students').on('change', function () {
+        var type = 'uncheck';
+        if ($(this).is(':checked')) {
+            type = 'check';
+        }
+
+        $.each($('input[name="students[]"]'), function (key, item) {
+            var checked = false;
+            var id = $(item).val();
+
+            if (type == 'check') {
+                checked = true;
+                addId(id);
+            } else {
+                removeId(id);
+            }
+
+            $(item).prop('checked', checked);
+        });
+    });
+
+    function removeId(id) {
+        var ids = $('#delete_form input').val().split(',');
+
+        ids = $.grep(ids, function (value) {
+            return value != id;
+        });
+
+        $('#delete_form input').val(ids.join(','));
+    }
+
+    function addId(id) {
+        var ids = $('#delete_form input').val().split(',');
+
+        if ($.inArray(id, ids) == -1) {
+            ids.push(id);
+        }
+
+        $('#delete_form input').val(ids.join(','));
+    }
 });
